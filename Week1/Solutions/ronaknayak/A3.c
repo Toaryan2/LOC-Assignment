afdd960201949f6a0bc087b8218157a9a8fe6e3b
Ronak Ashish Nayak <126500516+ronaknayak8@users.noreply.github.com>
2023-03-14 00:48:42 +0530

    TASK COMPLETED

---
+++ Week1/Solutions/ronaknayak/A3.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main() 
+
+{
+    int n, a, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    do 
+    
+    {
+        a = n%10;
+        sum = sum + a;
+        n = n/10;
+    } 
+    
+  while(n!= 0);
+
+    printf("The sum of digits is %d.", sum);
+
+    return 0;
+}
---
+++ Week1/Solutions/ronaknayak/A4.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+int main() 
+
+
+{
+    float n1, n2;
+   
+    printf("Enter two numbers: ");
+    scanf("%f%f", &n1, &n2);
+
+    
+    switch(1) {
+        case 1 :
+            printf("%.2f + %.2f = %.2f\n", n1, n2, n1 + n2);
+            
+        case 2 :
+            printf("%.2f - %.2f = %.2f\n", n1, n2, n1 - n2);
+            
+        case 3 :
+            printf("%.2f * %.2f = %.2f\n", n1, n2, n1 * n2);
+            
+        case 4 :
+            if(n2 == 0) {
+                printf("Error: Cannot divide by zero.\n");
+            }
+            else {
+                printf("%.2f / %.2f = %.2f\n", n1, n2, n1 / n2);
+            }
+            break;
+        default:
+            printf("Enter valid input\n");
+    }
+
+    return 0;
+}
